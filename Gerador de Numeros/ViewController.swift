9a8c1b7ebcdbc5c4b0170a547d5d44661f6d2a81
natashagp <natasha.paulon@gmail.com>
2020-10-12 11:52:33 -0300

    created app to randomly choose a number

--- Gerador de Numeros/ViewController.swift
+++ Gerador de Numeros/ViewController.swift
@@ -8,6 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    @IBAction func gerarNumero(_ sender: Any) {
+        
+        var numero = arc4random_uniform(11)
+        legendaResultado.text = String(numero)
+        
+    }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
